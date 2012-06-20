b4f717b7b5a733cbb4323f7c6fbc97ce3d1edee9
a-know <a.know.3373@gmail.com>
2012-06-20 10:57:37 +0900

    Chapter 14.

---
+++ src/ChainOfResponsibility/Main.java
@@ -0,0 +1,24 @@
+package ChainOfResponsibility;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Support alice = new NoSupport("Alice");
+		Support bob = new LimitSupport("Bob", 100);
+		Support charlie = new SpecialSupport("Charile", 429);
+		Support diana = new LimitSupport("Diana", 200);
+		Support elmo = new OddSupport("Elmo");
+		Support fred = new LimitSupport("Fred", 300);
+
+		alice.setNext(bob).setNext(charlie).setNext(diana).setNext(elmo).setNext(fred);
+
+		//トラブルを発生
+		for(int i = 0; i < 500; i += 33){
+			alice.support(new Trouble(i));
+		}
+	}
+
+}
